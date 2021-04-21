c5d9ea937cf77b21045c58c896812238d808359a
YaskurBearlyFernandes <12050111676@students.uin-suska.ac.id>
2021-04-21 21:04:09 +0700

    Nama : Yaskur Bearly Fernandes Nim  : 12050111676 Kelas: 2H

---
+++ src/NIMRun.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class NIMRun {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nama: "); //String nama = "Yaskur Bearly Fernandes";
+        String nama = input.nextLine();
+        System.out.print("NIM : "); //String nim = "12050111676";
+        String nim = input.nextLine();
+
+        NIM objNim = new NIM();
+        objNim.setNim(nim);
+        System.out.println("Jenjang Perndidikan = "+objNim.getJenjang());
+        System.out.println("Tahun Masuk = 20"+objNim.getTahun());
+        System.out.println("Fakultas = "+objNim.getFakultas());
+        System.out.println("Jurusan = "+objNim.getJurusan());
+        System.out.println("Jenis kelamin = "+objNim.getJenisKelamin());
+        System.out.println("Nomor Urut Mahasiswa = "+objNim.getNomorurut());
+    }
+}
